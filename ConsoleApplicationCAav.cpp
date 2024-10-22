467dea4c79769a5b0b7b0ca2195ff285fd9d2add
Andy Duverneau <77910275+andydu001@users.noreply.github.com>
2024-10-22 14:28:58 -0400

    Add files via upload

---
+++ ConsoleApplicationCAav.cpp
@@ -0,0 +1,11 @@
+/*
+
+This program is a simple remainder that I can get away with C Plus Plus.
+*/
+#include <iostream>
+int main()
+{ 
+	std::cout<<__std_get_string_size_without_trailing_whitespace("Andy Duverneau", 14);
+}
+
+ 
